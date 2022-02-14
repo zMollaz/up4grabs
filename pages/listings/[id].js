90576ee1beeaa16a9dcb0202eb5500c9e98c744c
Mikko De Los Reyes <mikkode212@gmail.com>
2022-02-14 15:49:43 -0500

    listingItem now displays unique title

--- pages/listings/[id].js
+++ pages/listings/[id].js
@@ -34,8 +34,8 @@ export default function listingItem({ listingItem }) {
     <div className="lg:w-4/5 mx-auto flex flex-wrap">
       <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src="https://www.whitmorerarebooks.com/pictures/medium/2465.jpg"/>
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+        <h2 className="text-sm title-font text-gray-500 tracking-widest">Up4Grabs</h2>
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{title}</h1>
         <div className="flex mb-4">
   
           <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
@@ -64,7 +64,7 @@ export default function listingItem({ listingItem }) {
             </div>
           </div>
         <div className="flex">
-          <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
+          <span className="title-font font-medium text-2xl text-gray-900">Like to bid</span>
           <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Button</button>
           <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
