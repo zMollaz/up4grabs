b322f6f995f26d7f9a2a45eaba8d74e54c299126
Mikko De Los Reyes <mikkode212@gmail.com>
2022-02-14 15:53:04 -0500

    itemListing now displays unique description

--- pages/listings/[id].js
+++ pages/listings/[id].js
@@ -32,7 +32,7 @@ export default function listingItem({ listingItem }) {
 <section className="text-gray-700 body-font overflow-hidden bg-white">
   <div className="container px-5 py-24 mx-auto">
     <div className="lg:w-4/5 mx-auto flex flex-wrap">
-      <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src="https://www.whitmorerarebooks.com/pictures/medium/2465.jpg"/>
+      <img alt="ecommerce" className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={img_src}/>
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
         <h2 className="text-sm title-font text-gray-500 tracking-widest">Up4Grabs</h2>
         <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{title}</h1>
@@ -56,7 +56,7 @@ export default function listingItem({ listingItem }) {
             </a>
           </span>
         </div>
-        <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+        <p className="leading-relaxed">{description}</p>
         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
           <div className="flex">
           </div>
