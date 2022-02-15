b125ca6ad8f8cf5df51ae13bf384c8fcb603eaa9
A E <aelmollaz@gmail.com>
2022-02-14 22:49:47 -0500

    Fix listing page color

--- pages/listings/[id].js
+++ pages/listings/[id].js
@@ -33,16 +33,16 @@ export default function listingItem({ listingItem }) {
               src={img_src}
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-              <h2 className="text-sm title-font text-gray-500 tracking-widest">
+              <h2 className="text-sm title-font text-gray-dark tracking-widest">
                 Up4Grabs
               </h2>
-              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+              <h1 className="text-gray-dark text-3xl title-font font-medium mb-1">
                 {title}
               </h1>
               <div className="flex mb-4">
                 <Countdown end_date={end_date} />
-                <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
-                  <a className="text-gray-500">
+                <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-light">
+                  <a className="text-gray-dark">
                     <svg
                       fill="currentColor"
                       strokeLinecap="round"
@@ -54,7 +54,7 @@ export default function listingItem({ listingItem }) {
                       <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                     </svg>
                   </a>
-                  <a className="ml-2 text-gray-500">
+                  <a className="ml-2 text-gray-dark">
                     <svg
                       fill="currentColor"
                       strokeLinecap="round"
@@ -66,7 +66,7 @@ export default function listingItem({ listingItem }) {
                       <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                     </svg>
                   </a>
-                  <a className="ml-2 text-gray-500">
+                  <a className="ml-2 text-gray-dark">
                     <svg
                       fill="currentColor"
                       strokeLinecap="round"
@@ -80,13 +80,13 @@ export default function listingItem({ listingItem }) {
                   </a>
                 </span>
               </div>
-              <p className="leading-relaxed">{description}</p>
-              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+              <p className="leading-relaxed text-gray-dark">{description}</p>
+              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-dark mb-5">
                 <div className="flex"></div>
                 <div className="relative"></div>
               </div>
               <div className="flex">
-                <span className="title-font font-medium text-2xl text-gray-900">
+                <span className="title-font font-medium text-2xl text-gray-dark">
                   Like to bid
                 </span>
                 <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
