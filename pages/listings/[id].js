dfc2313b02dff7ad420212c3cf66fd856029fecc
A E <aelmollaz@gmail.com>
2022-02-15 09:26:41 -0500

    Restore getservside logic

--- pages/listings/[id].js
+++ pages/listings/[id].js
@@ -8,7 +8,7 @@ import axios from "axios";
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiYWVsbW9sbGF6IiwiYSI6ImNremJpcmY4ZDJlbjIyb28yZWt3NjF5MmMifQ.03oFENowylydeoRfp732qg";
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   const listingItem = await prisma.listings.findUnique({
     where: {
       id: Number(context.params.id),
@@ -24,7 +24,6 @@ export async function getStaticProps(context) {
 
   return {
     props: { listingItem, coordinates, },
-    revalidate: 10,
   };
 }
 
