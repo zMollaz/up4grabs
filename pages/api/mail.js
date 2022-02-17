94deda4e8c7f1cb4d4394e959513289eafdb3f81
Mikko De Los Reyes <mikkode212@gmail.com>
2022-02-17 14:20:05 -0500

    created mail.js api, and moved sendgrid code to it

---
+++ pages/api/mail.js
@@ -0,0 +1,19 @@
+const sgMail = require('@sendgrid/mail')
+
+const API_KEY = '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'
+
+sgMail.setApiKey(API_KEY)
+
+const message = {
+  to: 'mikko.delosreyes12@gmail.com',
+  from: 'up4grabs.app@gmail.com',
+  subject: "You're a winner baby!",
+  text: "You've won an item from Bobby Lee",
+  html: "<h1>You've won an item from Bobby Lee</h1>"
+}
+
+sgMail
+  .send(message)
+  .then((response) => console.log('Email sent!'))
+  .catch((error) => console.log(error.message));
+
