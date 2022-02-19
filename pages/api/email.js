91eb0f44939eddfec50cfb86af15aa0f920f0186
A E <aelmollaz@gmail.com>
2022-02-18 22:15:37 -0500

    Add second email api

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,6 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
-// const { apiKey } = require('../../config.js');
+
 export default async function emailHandler(req, res) {
   if (req.method !== "GET" && req.method !== "POST") {
     return res.status(405).json({ messsage: "Method not allowed" });
@@ -11,24 +11,28 @@ export default async function emailHandler(req, res) {
     sgMail.setApiKey(apiKey);
 
     const message = {
-      to: "mikko.delosreyes12@gmail.com",
       // to: req.body.winner.email,
-      from: "up4grabs.app1@gmail.com",
+      to: "mikko.delosreyes12@gmail.com",
+      // from: "up4grabs.app1@gmail.com",
+      from: "up4grabs.app2@gmail.com",
       isMultiple: false,
       dynamicTemplateData: {
         winner: req.body.winner?.name,
         listingTitle: req.body.listingTitle,
       },
-      template_id: "d-9c1463d498324294b040e2b08e5c3313",
+      // template_id: "d-9c1463d498324294b040e2b08e5c3313",  //template 1
+      template_id: "d-9b54ba70537b492192ade1c98b7df59b",     //template 2
     };
 
     console.log("123", message);
 
     sgMail
       .send(message)
-      .then((response) => console.log("Email sent!"))
+      .then((response) => {
+        console.log("Email sent!")
+        res.json({ response });
+    })
       .catch((error) => console.log(error.message));
 
-    // res.json({ response });
   }
 }
