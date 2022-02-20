cd753b9bdfcc84cf9a9896bca5001bd0eda63aef
A E <aelmollaz@gmail.com>
2022-02-19 20:59:54 -0500

    Fix add listing while on likes page

--- pages/users/likes.js
+++ pages/users/likes.js
@@ -5,33 +5,36 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import prisma from "../../lib/prisma";
 import { useContext, useEffect, useState } from "react";
 import { UsersContext } from "../../context/UsersContext";
+import { ListingsContext } from "../../context/ListingsContext";
+import useListings from "../../hooks/useListings";
 
 export async function getServerSideProps() {
-  const listings = await prisma.listings.findMany();
-  const likes = await prisma.biddings.findMany();
+  const defaultListings = await prisma.listings.findMany();
+  const defaultLikes = await prisma.biddings.findMany();
 
   return {
-    props: { listings, likes },
+    props: { defaultListings, defaultLikes },
   };
 }
 
-export default function UserLikes({ listings, likes }) {
-
+export default function UserLikes(props) {
   const { user } = useContext(UsersContext);
-  const [view, setView] = useState(listings);
+  const [view, setView] = useState(props.defaultListings);
   const myDate = function (date) {
     dayjs.extend(relativeTime);
     return dayjs(date).fromNow();
   };
 
-  const filteredLikes = likes.filter((like) => like.user_id === user.id);
+  const filteredLikes = props.defaultLikes.filter(
+    (like) => like.user_id === user.id
+  );
 
   useEffect(() => {
-      const listingsArr = filteredLikes.map((like) => like.listing_id);
-      const userListings = listings.filter((listing) =>
-        listingsArr.includes(listing.id)
-      );
-      setView(userListings);
+    const listingsArr = filteredLikes.map((like) => like.listing_id);
+    const userListings = props.defaultListings.filter((listing) =>
+      listingsArr.includes(listing.id)
+    );
+    setView(userListings);
   }, [user]);
 
   const parsedListings = view.map((listing) => {
@@ -47,21 +50,23 @@ export default function UserLikes({ listings, likes }) {
   });
 
   return (
-    <Layout>
-      <div
-        aria-hidden="true"
-        className="max-w-fill h-full relative overflow-y-auto overflow-x-auto right-0 left-0 top-4 z-100 justify-center items-center h-modal md:h-full md:inset-0"
-      >
-        <div className="relative inset-0  w-full  h-full md:h-auto">
-          <div className="bg-off-white h-full pr-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-6 ">
-            {filteredLikes.length > 0 ? (
-              parsedListings
-            ) : (
-              <h1>You currently have no biddings to display</h1>
-            )}
+    <ListingsContext.Provider value={useListings(props)}>
+      <Layout>
+        <div
+          aria-hidden="true"
+          className="max-w-fill h-full relative overflow-y-auto overflow-x-auto right-0 left-0 top-4 z-100 justify-center items-center h-modal md:h-full md:inset-0"
+        >
+          <div className="relative inset-0  w-full  h-full md:h-auto">
+            <div className="bg-off-white h-full pr-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 md:gap-4 lg:gap-6 ">
+              {filteredLikes.length > 0 ? (
+                parsedListings
+              ) : (
+                <h1>You currently have no biddings to display</h1>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </ListingsContext.Provider>
   );
 }
